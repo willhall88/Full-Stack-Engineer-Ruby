6318afb413b16d7e670b60c3156f331f6cee695c
Will Hall <will.hall@factorymedia.com>
2017-02-19 18:27:15 +0000

    Add cover images

--- app/features/comics_index.feature
+++ app/features/comics_index.feature
@@ -8,3 +8,4 @@ And they should be ordered from most recent to the oldest
 Scenario: Viewing comics
 When I am on the homepage
 Then I should see a list of comic book titles
+And I should see each comics cover picture
--- app/features/step_definitions/comic_index_steps.rb
+++ app/features/step_definitions/comic_index_steps.rb
@@ -1,9 +1,14 @@
 When(/^I am on the homepage$/) do
   visit '/'
+  @comics = ComicFetch.new.result_data
 end
 
 Then(/^I should see a list of comic book titles$/) do
-  @comics = ComicFetch.new.result_data
   elements = all('.c-comic__title')
+  expect(elements.length).to eq 20
+end
+
+Then(/^I should see each comics cover picture$/) do
+  elements = all('.c-comic__cover-image')
   expect(elements.length).to eq 20
 end
